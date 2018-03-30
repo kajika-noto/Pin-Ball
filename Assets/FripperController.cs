5e51d8737dd9c1067ee327761de8682b34a9e0eb
kajika-noto <wgwwb1017@yahoo.co.jp>
2018-03-30 23:39:27 +0900

    [Add] Unity-3Dゲーム「PinBall」
    【課題】得点の表示 ＆ 【発展課題】スマホのマルチタッチ対応化

---
+++ Assets/FripperController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+    private HingeJoint myHingeJoint;
+
+    private float defaultAngle = 20;
+    private float flickAngle = -20;
+
+    //FingerID格納
+    private int leftFingerId = 0;
+    private int rightFingerId = 0;
+
+	// Use this for initialization
+	void Start () {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+        SetAngle(this.defaultAngle);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //キーボード操作
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+		if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+
+        //タッチ操作
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            var id = Input.touches[i].fingerId;
+            var pos = Input.touches[i].position;
+
+            switch (Input.touches[i].phase)
+            {
+                case TouchPhase.Began:
+                    if (pos.x < Screen.width * 0.5 && tag == "LeftFripperTag")
+                    {
+                        SetAngle(this.flickAngle);
+                        leftFingerId = id;
+                    }
+                    if (pos.x > Screen.width * 0.5 && tag == "RightFripperTag")
+                    {
+                        SetAngle(this.flickAngle);
+                        rightFingerId = id;
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    if (id == leftFingerId && tag == "LeftFripperTag")
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                    if (id == rightFingerId && tag == "RightFripperTag")
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                    break;
+            }
+        }
+
+	}
+
+    public void SetAngle (float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+    private int score = 000;
+    private int largestar = 10;
+    private int smallstar = 1;
+    private int largecloud = 50;
+    private int smallcloud = 20;
+    private GameObject scorepointText;
+
+    int Add(int addpoint)
+    {
+        score += addpoint;
+        return score;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        this.scorepointText = GameObject.Find("ScorePointText");
+        
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (score <= 0)
+        {
+            this.scorepointText.GetComponent<Text>().text = "000";
+        } else
+        {
+            this.scorepointText.GetComponent<Text>().text = score.ToString("000");
+        }
+        
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "LargeStarTag")
+        {
+            Add(largestar); 
+        }
+        else if (collision.gameObject.tag == "SmallStarTag")
+        {
+            Add(smallstar);
+        }
+        else if (collision.gameObject.tag == "LargeCloudTag")
+        {
+            Add(largecloud);
+        }
+        else if (collision.gameObject.tag == "SmallCloudTag")
+        {
+            Add(smallcloud);
+         }
+
+    }
+
+
+}
